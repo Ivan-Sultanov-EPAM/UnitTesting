2398a4bbbaf121e0e7cdc7d76158aba4344cd4dc
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-12-04 13:20:21 +0600

    Add test for empty string

--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -5,9 +5,16 @@ namespace StringCalculator.Tests
     public class StringCalculatorTests
     {
         [Fact]
-        public void Test1()
+        public void Should_Return_0_If_Empty_String_Passed()
         {
+            //Arrange
+            var emptyStringArgument = "";
 
+            //Act
+            var result = Calculator.Add(emptyStringArgument);
+
+            //Assert
+            Assert.Equal(0, result);
         }
     }
 }
