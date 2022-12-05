216ced033048d4231fe4132d6b577ffbd1f50a33
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-12-05 13:28:40 +0600

    Implement method for printing digits from 0 to 9

--- LCDDigits/LCDConverter.cs
+++ LCDDigits/LCDConverter.cs
@@ -1,6 +1,46 @@
-﻿namespace LCDDigits
+﻿using System.Collections.Generic;
+
+namespace LCDDigits
 {
     public class LCDConverter
     {
+        private static readonly Dictionary<int, string> Digits = new Dictionary<int, string>()
+        {
+            { 0, " _ \n" +
+                 "| |\n" +
+                 "|_|" },
+            { 1, "   \n" +
+                 "  |\n" +
+                 "  |" },
+            { 2, " _ \n" +
+                 " _|\n" +
+                 "|_ " },
+            { 3, " _ \n" +
+                 " _|\n" +
+                 " _|" },
+            { 4, "   \n" +
+                 "|_|\n" +
+                 "  |" },
+            { 5, " _ \n" +
+                 "|_ \n" +
+                 " _|" },
+            { 6, " _ \n" +
+                 "|_ \n" +
+                 "|_|" },
+            { 7, " _ \n" +
+                 "  |\n" +
+                 "  |" },
+            { 8, " _ \n" +
+                 "|_|\n" +
+                 "|_|" },
+            { 9, " _ \n" +
+                 "|_|\n" +
+                 "  |" }
+        };
+
+        public static string Convert(int digit)
+        {
+            return Digits[digit];
+        }
     }
 }
