0bfe7c2e8169325ec18713b19ceb1919e8b112ba
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-12-08 22:50:48 +0600

    Add implementation for handling invalid input

--- MineFields/MineFields.cs
+++ MineFields/MineFields.cs
@@ -1,10 +1,20 @@
-﻿namespace MineFields
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MineFields
 {
     public class MineFields
     {
+        private const string Pattern = @"^[*.]{4}\n[*.]{4}\n[*.]{4}$";
+
         public static string Process(string inputField)
         {
-            throw new System.NotImplementedException();
+            if (!Regex.IsMatch(inputField, Pattern))
+            {
+                throw new ArgumentException("Input value doesn't match required pattern");
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
