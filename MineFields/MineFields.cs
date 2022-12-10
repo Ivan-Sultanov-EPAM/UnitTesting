6cc4f98b0fd0a960b4d4c21ff80ae8a8ed126dc0
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-12-10 22:47:44 +0600

    Add implementation to produce correct output

--- MineFields/MineFields.cs
+++ MineFields/MineFields.cs
@@ -14,7 +14,105 @@ namespace MineFields
                 throw new ArgumentException("Input value doesn't match required pattern");
             }
 
-            throw new NotImplementedException();
+            var stringArray = new string[3][];
+            stringArray[0] = new string[4];
+            stringArray[1] = new string[4];
+            stringArray[2] = new string[4];
+
+            var fieldParts = inputField.Split("\n");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    stringArray[i][j] = fieldParts[i][j].ToString();
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (stringArray[i][j] == ".")
+                    {
+                        stringArray[i][j] = "0";
+                    }
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (stringArray[i][j] == "*")
+                    {
+                        ProcessMine(i, j);
+                    }
+                }
+            }
+
+            void ProcessMine(int i, int j)
+            {
+                if (j + 1 < 4 && stringArray[i][j + 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i][j + 1]);
+                    temp += 1;
+                    stringArray[i][j + 1] = temp.ToString();
+                }
+
+                if (j - 1 > -1 && stringArray[i][j - 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i][j - 1]);
+                    temp += 1;
+                    stringArray[i][j - 1] = temp.ToString();
+                }
+
+                if (i + 1 < 3 && stringArray[i + 1][j] != "*")
+                {
+                    var temp = int.Parse(stringArray[i + 1][j]);
+                    temp += 1;
+                    stringArray[i + 1][j] = temp.ToString();
+                }
+
+                if (i - 1 > -1 && stringArray[i - 1][j] != "*")
+                {
+                    var temp = int.Parse(stringArray[i - 1][j]);
+                    temp += 1;
+                    stringArray[i - 1][j] = temp.ToString();
+                }
+
+                if (i + 1 < 3 && j + 1 < 4 && stringArray[i + 1][j + 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i + 1][j + 1]);
+                    temp += 1;
+                    stringArray[i + 1][j + 1] = temp.ToString();
+                }
+
+                if (i - 1 > -1 && j - 1 > -1 && stringArray[i - 1][j - 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i - 1][j - 1]);
+                    temp += 1;
+                    stringArray[i - 1][j - 1] = temp.ToString();
+                }
+
+                if (i + 1 < 3 && j - 1 > -1 && stringArray[i + 1][j - 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i + 1][j - 1]);
+                    temp += 1;
+                    stringArray[i + 1][j - 1] = temp.ToString();
+                }
+
+                if (i - 1 > -1 && j + 1 < 4 && stringArray[i - 1][j + 1] != "*")
+                {
+                    var temp = int.Parse(stringArray[i - 1][j + 1]);
+                    temp += 1;
+                    stringArray[i - 1][j + 1] = temp.ToString();
+                }
+            }
+
+            return $"{string.Concat(stringArray[0])}\n" +
+                   $"{string.Concat(stringArray[1])}\n" +
+                   $"{string.Concat(stringArray[2])}";
         }
     }
 }
