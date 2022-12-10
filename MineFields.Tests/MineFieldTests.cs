ea8daf6e4645c503c44699a4690758dee947fd6f
Ivan Sultanov <Ivan_Sultanov@epam.com>
2022-12-10 22:45:50 +0600

    Add test should produce correct output

--- MineFields.Tests/MineFieldTests.cs
+++ MineFields.Tests/MineFieldTests.cs
@@ -18,5 +18,54 @@ namespace MineFields.Tests
             var exception = Assert.Throws<ArgumentException>(action);
             Assert.Equal("Input value doesn't match required pattern", exception.Message);
         }
+
+        [Theory]
+        [InlineData("*...\n" +
+                    "..*.\n" +
+                    "....",
+            "*211\n" +
+            "12*1\n" +
+            "0111")]
+        [InlineData("....\n" +
+                    "****\n" +
+                    "....",
+            "2332\n" +
+            "****\n" +
+            "2332")]
+        [InlineData("*..*\n" +
+                    "*..*\n" +
+                    "*..*",
+            "*22*\n" +
+            "*33*\n" +
+            "*22*")]
+        [InlineData("*.*.\n" +
+                    "....\n" +
+                    ".**.",
+            "*2*1\n" +
+            "2432\n" +
+            "1**1")]
+        [InlineData("****\n" +
+                    "****\n" +
+                    "****",
+            "****\n" +
+            "****\n" +
+            "****")]
+        [InlineData("....\n" +
+                    "....\n" +
+                    "....",
+            "0000\n" +
+            "0000\n" +
+            "0000")]
+        public void Should_Produce_Correct_Output(string input, string expected)
+        {
+            //Arrange
+            var inputString = input;
+
+            //Act
+            var result = MineFields.Process(inputString);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
